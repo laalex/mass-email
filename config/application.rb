88e2c87f544b9bb095523189858818c20b0926d4
Alexandru Lamba <roshkattu94@gmail.com>
2015-08-14 00:19:21 +0300

    changed app conf

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module MassMail
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += Ckeditor.assets
+    #config.assets.paths << "#{Rails.root}/vendor/assets"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
