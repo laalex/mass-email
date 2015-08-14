c1c158ad2da0127b1c626c6aefa3b276a6af7642
Alexandru Lamba <roshkattu94@gmail.com>
2015-08-14 14:48:01 +0300

    Added more line spacings

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,4 @@
 CKEDITOR.editorConfig = function (config) {
   config.extraPlugins = 'lineheight';
-  config.line_height="1;1.25;1.50;1.75;2;2.5;3;4;5;6;7;8;9;10";
+  config.line_height="0.2;0.5;0.7;0.8;0.9;1;1.25;1.50;1.75;2;2.5;3;4;5;6;7;8;9;10";
 }
