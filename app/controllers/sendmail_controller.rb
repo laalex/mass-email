6aeb4adca282d6446e68fcba8512a4c794ac9732
Alexandru Lamba <roshkattu94@gmail.com>
2015-08-25 21:33:58 +0300

    Added email attachments and forcing layout

--- app/controllers/sendmail_controller.rb
+++ app/controllers/sendmail_controller.rb
@@ -6,19 +6,43 @@ class SendmailController < ApplicationController
 
 
   def do_send
-    send_mail(params[:csv_file].tempfile.path)
-    flash[:notice] = "Your emails will be dispateched in background"
-    redirect_to root_url
+    if params[:csv_file].blank?
+      flash[:notice] = "Please select a CSV file containing email addressess"
+      redirect_to :back
+    else
+      # Upload the files first
+      uploaded_files = do_upload!(params[:attachment])
+      send_mail(params[:csv_file].tempfile.path, uploaded_files)
+      flash[:notice] = "Your emails will be dispateched in background"
+      redirect_to root_url
+    end
   end
 
 
   private
-    def send_mail(path)
+    def do_upload!(files_array)
+      return_array = []
+      dir = 'public/data'
+      if !files_array.blank?
+        files_array.each do |upload_file|
+          file_name = upload_file.original_filename
+          path = File.join(dir, file_name)
+          # write the file
+          File.open(path, "wb") { |f| f.write(upload_file.read) }
+          # Add the path into the return array
+          return_array << {name: file_name, path: path}
+        end
+      end
+      # Return array with uploaded files
+      return_array
+    end
+
+    def send_mail(path, attachments)
       CSV.foreach(path, :headers => true, :col_sep => ';') do |row|
         person = {firstName: row['FIRST_NAME'], lastName: row['LAST_NAME'], email: row['EMAIL']}
         if !person[:email].blank?
           mail = {message: params[:message][:content], subject: params[:subject]}
-          EmailSender.delay.send_email(person, mail)
+          EmailSender.delay.send_email(person, mail, attachments)
         end
       end
     end
--- app/mailers/email_sender.rb
+++ app/mailers/email_sender.rb
@@ -1,12 +1,19 @@
 class EmailSender < ApplicationMailer
   default from: "no-reply@icmla-conference.org"
 
-  def send_email(data, mail)
+  def send_email(data, mail, files_attached)
     @firstName = data[:firstName]
     @lastName = data[:lastName]
     @email = data[:email]
     @subject = mail[:subject]
     @message = mail[:message]
+    # Check if we have attachments
+    if !files_attached.blank?
+      files_attached.each do |file_attached|
+        attachments[file_attached[:name]] = File.read(file_attached[:path])
+      end
+    end
+    # Send the email
     mail(to: @email, subject: @subject)
   end
 end
